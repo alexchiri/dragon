60ca82ec5bc7669db9891b953c551aabea367dc8
Alexandru Chiritescu <alex@alexchiri.com>
2020-11-27 09:44:07 +0100

    Add CI release pipeline with travis.

---
+++ ci/before_deploy.sh
@@ -0,0 +1,35 @@
+# This script takes care of building your crate and packaging it for release
+
+set -ex
+
+main() {
+    local src=$(pwd) \
+          stage=
+
+    case $TRAVIS_OS_NAME in
+        linux)
+            stage=$(mktemp -d)
+            ;;
+        osx)
+            stage=$(mktemp -d -t tmp)
+            ;;
+    esac
+
+    test -f Cargo.lock || cargo generate-lockfile
+
+    cross rustc --bin dragon --target $TARGET --release -- -C lto
+
+    if test -f target/$TARGET/release/hmum; then
+      cp target/$TARGET/release/dragon $stage/
+    else
+      cp target/$TARGET/release/dragon.exe $stage/
+    fi
+
+    cd $stage
+    tar czf $src/$CRATE_NAME-$TRAVIS_TAG-$TARGET.tar.gz *
+    cd $src
+
+    rm -rf $stage
+}
+
+main
